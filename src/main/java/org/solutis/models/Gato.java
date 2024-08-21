c513ca382cf804b005df729466aa09942087b6d8
gerson.fragoso <gersonfragoso.araujo@gmail.com>
2024-08-21 12:06:01 -0300

    feat: create gato and leao model

--- src/main/java/org/solutis/models/Gato.java
+++ src/main/java/org/solutis/models/Gato.java
@@ -1,4 +1,24 @@
 package org.solutis.models;
 
-public class Gato {
+import org.solutis.abstracts.AnimalAB;
+
+public class Gato extends AnimalAB {
+    @Override
+    public void comer(int quantComida) {
+        this.quantComida += quantComida;
+        System.out.println("O gato comeu " + quantComida + " unidade de comida");
+
+    }
+
+    @Override
+    public void mover(int distancia) {
+        this.distancia += distancia;
+        System.out.println("O gato andou " + distancia + " metros");
+    }
+
+    @Override
+    public void dormir(int tempoDormido) {
+        this.tempoDormido += tempoDormido;
+        System.out.println("O gato dormiu " + tempoDormido + " horas");
+    }
 }
